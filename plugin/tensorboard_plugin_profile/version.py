0f64f3cfa73385dc8991471e65a3748e8bd189e3
Profiler Team <copybara-servicebot@google.com>
2025-06-03 10:49:58 -0700

    Release tensorboard-plugin-profile 2.19.8 to address the libstdc++ dependency issue.
    PiperOrigin-RevId: 766733082

--- plugin/tensorboard_plugin_profile/version.py
+++ plugin/tensorboard_plugin_profile/version.py
@@ -14,4 +14,4 @@
 # ==============================================================================
 """Version information for tensorboard-plugin-profile."""
 
-__version__ = "2.19.7"
+__version__ = "2.19.8"
