d65b34f5563aa7dfbc6f1e78e0421b47187eb943
Mudit Gokhale <muditgokhale@google.com>
2025-06-03 23:00:10 -0700

    Update input_pipeline to leverage new data service.
    PiperOrigin-RevId: 766986037

--- frontend/app/components/input_pipeline/input_pipeline.ts
+++ frontend/app/components/input_pipeline/input_pipeline.ts
@@ -1,11 +1,10 @@
-import {Component, OnDestroy} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {Component, inject, OnDestroy} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
 import {Store} from '@ngrx/store';
 import {STACK_CHART_FILL_COLORS} from 'org_xprof/frontend/app/common/constants/constants';
-import {InputPipelineDataTable, SimpleDataTable,} from 'org_xprof/frontend/app/common/interfaces/data_table';
-import {NavigationEvent} from 'org_xprof/frontend/app/common/interfaces/navigation_event';
+import {InputPipelineDataTable, SimpleDataTable, } from 'org_xprof/frontend/app/common/interfaces/data_table';
 import {setLoadingState} from 'org_xprof/frontend/app/common/utils/utils';
-import {DataService} from 'org_xprof/frontend/app/services/data_service/data_service';
+import {DATA_SERVICE_INTERFACE_TOKEN, type DataServiceV2Interface} from 'org_xprof/frontend/app/services/data_service_v2/data_service_v2_interface';
 import {setCurrentToolStateAction} from 'org_xprof/frontend/app/store/actions';
 import {ReplaySubject} from 'rxjs';
 import {takeUntil} from 'rxjs/operators';
@@ -13,11 +12,13 @@ import {takeUntil} from 'rxjs/operators';
 import {InputPipelineCommon} from './input_pipeline_common';
 
 const COLUMN_ID_MAX_INFEED_CORE = 'index';
+// TODO(muditgokhale) : Pass this as a property in Input Pipeline Data Table
+// once the DCU is complete.
 const STEPTIME_COLUMN_IDS_FOR_TPU_INTERNAL = [
   'stepnum',
   'tcComputeTimeMs',
   'scv0ComputeTimeMs',
-  'scv0InfeedTimeMs',
+  'scv0InfeedTimeMs,',
   'tcInfeedTimeMs',
   'tcOutfeedTimeMs',
   'tcIdleTimeMs',
@@ -32,30 +33,36 @@ const STEPTIME_COLUMN_IDS_FOR_TPU_INTERNAL = [
   styleUrls: ['./input_pipeline.css']
 })
 export class InputPipeline extends InputPipelineCommon implements OnDestroy {
-  readonly tool = 'input_pipeline_analyzer';
+  private readonly dataService: DataServiceV2Interface =
+      inject(DATA_SERVICE_INTERFACE_TOKEN);
+
+  tool = 'input_pipeline';
+  sessionId = '';
+  host = '';
   readonly columnIds = STEPTIME_COLUMN_IDS_FOR_TPU_INTERNAL;
   readonly columnColors = STACK_CHART_FILL_COLORS;
   /** Handles on-destroy Subject, used to unsubscribe. */
   private readonly destroyed = new ReplaySubject<void>(1);
 
-  constructor(
-      route: ActivatedRoute, private readonly dataService: DataService,
-      private readonly store: Store<{}>) {
+  constructor(route: ActivatedRoute, private readonly store: Store<{}>) {
     super();
     route.params.pipe(takeUntil(this.destroyed)).subscribe((params) => {
-      this.update(params as NavigationEvent);
+      this.processQuery(params);
+      this.update();
     });
     this.store.dispatch(setCurrentToolStateAction({currentTool: this.tool}));
   }
 
-  update(event: NavigationEvent) {
-    const run = event.run || '';
-    const tag = event.tag || this.tool;
-    const host = event.host || '';
+  processQuery(params: Params) {
+    this.tool = params['tag'] || this.tool;
+    this.sessionId = params['run'] || params['sessionId'] || this.sessionId;
+    this.host = params['host'] || this.host;
+  }
 
+  update() {
     setLoadingState(true, this.store, 'Loading input pipeline data');
 
-    this.dataService.getData(run, tag, host)
+    this.dataService.getData(this.sessionId, this.tool, this.host)
         .pipe(takeUntil(this.destroyed))
         .subscribe((data) => {
           setLoadingState(false, this.store);
