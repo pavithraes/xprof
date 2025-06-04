3268a6eba1e26cace228952f58efa994ad127341
Profiler Team <copybara-servicebot@google.com>
2025-06-04 10:17:38 -0700

    Release tensorboard-plugin-profile 2.19.9. This release will make sure xprof work with old version python 3.9 and forward.
    PiperOrigin-RevId: 767194389

--- plugin/tensorboard_plugin_profile/version.py
+++ plugin/tensorboard_plugin_profile/version.py
@@ -14,4 +14,4 @@
 # ==============================================================================
 """Version information for tensorboard-plugin-profile."""
 
-__version__ = "2.19.8"
+__version__ = "2.19.9"
