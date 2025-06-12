2bb1d119303bf339d9634fb359f89a52b73a3e07
Mudit Gokhale <muditgokhale@google.com>
2025-06-12 08:21:27 -0700

    Add max heap chart downloader
    PiperOrigin-RevId: 770663593

---
+++ frontend/app/components/memory_viewer/max_heap_chart_downloader/max_heap_chart_downloader.ts
@@ -0,0 +1,98 @@
+import {Component, Input} from '@angular/core';
+import {SafeDownloader} from 'google3/javascript/security/safe_downloader';
+import {type MemoryViewerPreprocessResult} from 'org_xprof/frontend/app/common/interfaces/data_table';
+import {HeapObject} from 'org_xprof/frontend/app/common/interfaces/heap_object';
+import {MemoryUsage} from 'org_xprof/frontend/app/components/memory_viewer/memory_usage/memory_usage';
+import {writer} from 'ts-csv';
+
+/** A component to download hlo module in proto, text or json formats. */
+@Component({
+  standalone: false,
+  selector: 'max-heap-chart-downloader',
+  templateUrl: './max_heap_chart_downloader.ng.html',
+  styleUrls: ['./max_heap_chart_downloader.scss'],
+  providers: [],
+})
+export class MaxHeapChartDownloader {
+  /** Preprocessed result for memory viewer */
+  @Input()
+  memoryViewerPreprocessResult: MemoryViewerPreprocessResult | null = null;
+
+  /** XLA memory space color */
+  @Input() memorySpaceColor = '0';
+
+  /** Heap objects to download. */
+  heapObjects: HeapObject[] = [];
+  /** Timespan (alloc and free) for logical buffer. */
+  logicalBufferSpans: {[key: number]: number[]} = {};
+  /** Heap size sequence. */
+  heapSizes: number[] = [];
+
+  private readonly data: string[][] = [];
+  sessionId = '';
+
+  private getLogicalBufferSpan(index?: number): [number, number] {
+    const bufferSpan: [number, number] = [0, 0];
+    if (index) {
+      const span = this.logicalBufferSpans[index];
+      if (span) {
+        bufferSpan[0] = span[0];
+        bufferSpan[1] = span[1] < 0 ? this.heapSizes.length - 1 : span[1];
+      } else {
+        bufferSpan[1] = this.heapSizes.length - 1;
+      }
+    }
+    return bufferSpan;
+  }
+
+  async downloadMaxHeapChart() {
+    const usage = new MemoryUsage(
+      this.memoryViewerPreprocessResult,
+      Number(this.memorySpaceColor),
+      null,
+      null,
+      null,
+    );
+    if (usage.diagnostics.errors.length > 0) {
+      console.error(usage.diagnostics.errors[0]);
+      return;
+    }
+
+    this.heapObjects = usage.maxHeap || [];
+    this.logicalBufferSpans = usage.logicalBufferSpans || {};
+    this.heapSizes = usage.heapSizes || [];
+
+    const moduleName = this.memoryViewerPreprocessResult?.moduleName || '';
+    const fileName = moduleName + '.csv';
+    this.data.push([
+      'InstructionName',
+      'UnpaddedSizeMiB',
+      'SizeMiB',
+      'AllocAt',
+      'FreeAt',
+      'TfOpName',
+      'OpCode',
+      'Shape',
+      'AllocationType',
+    ]);
+    for (const heapObject of this.heapObjects) {
+      const span = this.getLogicalBufferSpan(heapObject.logicalBufferId);
+      this.data.push([
+        heapObject.instructionName || 'UNKNOWN',
+        heapObject.unpaddedSizeMiB
+          ? heapObject.unpaddedSizeMiB.toString()
+          : 'UNKNOWN',
+        heapObject.sizeMiB ? heapObject.sizeMiB.toString() : 'UNKNOWN',
+        span[0].toString(),
+        span[1].toString(),
+        heapObject.tfOpName || 'UNKNOWN',
+        heapObject.opcode || 'UNKNOWN',
+        heapObject.shape || 'UNKNOWN',
+        heapObject.groupName || 'UNKNOWN',
+      ]);
+    }
+    const csvString = [...writer(this.data)].join('');
+    const csvBlob = new Blob([csvString], {type: 'text/csv;charset=utf-8'});
+    await SafeDownloader.download(csvBlob, fileName, 'text/csv;charset=utf-8');
+  }
+}
