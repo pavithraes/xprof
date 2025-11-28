902666d26e3b9095a8af8443ca37e50b0e6d0bbf
Subham Soni <subhamsoni@google.com>
2025-11-27 22:05:20 -0800

    Update xprof version to 2.21.2
    PiperOrigin-RevId: 837708193

--- plugin/xprof/version.py
+++ plugin/xprof/version.py
@@ -14,4 +14,4 @@
 # ==============================================================================
 """Version information for tensorboard-plugin-profile."""
 
-__version__ = "2.21.1"
+__version__ = "2.21.2"
