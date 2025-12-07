687395f7897bf029ead26b2412b7d153f2626368
Mudit Gokhale <muditgokhale@google.com>
2025-12-07 12:41:46 -0800

    Hide capture button from empty page for fully managed
    PiperOrigin-RevId: 841466987

--- frontend/app/components/empty_page/empty_page.ts
+++ frontend/app/components/empty_page/empty_page.ts
@@ -1,5 +1,9 @@
 import 'org_xprof/frontend/app/common/interfaces/window';
-import {Component} from '@angular/core';
+
+import {Component, inject} from '@angular/core';
+import {DATA_SERVICE_INTERFACE_TOKEN, DataServiceV2Interface} from 'org_xprof/frontend/app/services/data_service_v2/data_service_v2_interface';
+import {firstValueFrom, ReplaySubject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 /** An empty page component. */
 @Component({
@@ -9,5 +13,29 @@ import {Component} from '@angular/core';
   styleUrls: ['./empty_page.css']
 })
 export class EmptyPage {
+  private readonly destroyed = new ReplaySubject<void>(1);
+
+  hideCaptureProfileButton = true;
+
+  private readonly dataService: DataServiceV2Interface =
+      inject(DATA_SERVICE_INTERFACE_TOKEN);
+
   inColab = !!(window.parent.TENSORBOARD_ENV || {}).IN_COLAB;
+
+   ngOnInit() {
+    this.fetchProfilerConfig();
+  }
+
+  async fetchProfilerConfig() {
+    const config = await firstValueFrom(
+        this.dataService.getConfig().pipe(takeUntil(this.destroyed)));
+    if (config) {
+      this.hideCaptureProfileButton = config.hideCaptureProfileButton;
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
 }
